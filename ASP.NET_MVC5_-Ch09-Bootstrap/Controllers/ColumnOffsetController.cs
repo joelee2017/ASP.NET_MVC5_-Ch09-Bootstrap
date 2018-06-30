0cd94a8bf6ef3edc087b9b9e02d859100566b178
joe lee <as955133@gmail.com>
2018-06-30 15:31:57 +0800

    ASP.NET MVC 與 Bootstrap 合壁 自行編寫 Bootstrap BS.cshtml

---
+++ ASP.NET_MVC5_-Ch09-Bootstrap/Controllers/ColumnOffsetController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC5__Ch09_Bootstrap.Controllers
+{
+    public class ColumnOffsetController : Controller
+    {
+        // GET: ColumnOffset
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BS()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET_MVC5_-Ch09-Bootstrap/Controllers/NestcolumnsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC5__Ch09_Bootstrap.Controllers
+{
+    public class NestcolumnsController : Controller
+    {
+        // GET: Nestcolumns
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
