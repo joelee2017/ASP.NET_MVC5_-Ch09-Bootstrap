2e906fbacaea122c1516c4d5be0ffc54045b96f4
joe lee <as955133@gmail.com>
2018-06-30 14:22:47 +0800

    加入專案檔案。

---
+++ ASP.NET_MVC5_-Ch09-Bootstrap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC5__Ch09_Bootstrap.MvcApplication" Language="C#" %>
---
+++ ASP.NET_MVC5_-Ch09-Bootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC5__Ch09_Bootstrap.Startup))]
+namespace ASP.NET_MVC5__Ch09_Bootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
